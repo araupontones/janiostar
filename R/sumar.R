2959c05caaabf73426041ca5952b72f798db23e2
araupontones <43657130+araupontones@users.noreply.github.com>
2025-01-21 10:08:39 +0100

    v1

---
+++ R/sumar.R
@@ -0,0 +1,14 @@
+#' @title Sums
+#' @description
+#' This funcions sums the values x and y and returns the sum
+#'
+#' @param x es un numero
+#' @param y es otro numero
+
+sumar <- function(x,y){
+
+  suma = x + y
+
+  return(suma)
+
+}
